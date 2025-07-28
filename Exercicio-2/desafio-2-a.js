b666908ca046c87109cc0e18a9d36294572357fa
mariana4ads <mariana4ads@gmail.com>
2025-07-28 02:13:13 -0300

    resposta exercicio 2 desafio a

---
+++ Exercicio-2/desafio-2-a.js
@@ -0,0 +1,16 @@
+// a. Use map para criar um array de strings formatadas como "Nome do produto - Categoria" para cada produto
+
+
+const products = [
+  { id: 1, name: "Notebook", price: 1200, category: "Eletrônicos" },
+  { id: 2, name: "Camiseta", price: 25, category: "Vestuário" },
+  { id: 3, name: "Cafeteira", price: 100, category: "Cozinha" },
+  { id: 4, name: "Fones de Ouvido", price: 80, category: "Eletrônicos" },
+  { id: 5, name: "Calça Jeans", price: 45, category: "Vestuário" },
+];
+
+const productInfo = products.map((product) => {
+  return `${product.name} - ${product.category}`
+});
+
+console.log(productInfo);
