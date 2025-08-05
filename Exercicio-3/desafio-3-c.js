69513098519427b3d2614636e93a58ae9791815f
mariana4ads <mariana4ads@gmail.com>
2025-08-04 21:25:28 -0300

    respostas exercicio 3 desafio c

---
+++ Exercicio-3/desafio-3-c.js
@@ -0,0 +1,13 @@
+// c. Use filter para encontrar estudantes que tiraram pelo menos uma nota abaixo de 70
+
+const students = [
+  { id: 1, name: "João", age: 20, grades: [85, 90, 78], active: true },
+  { id: 2, name: "Maria", age: 19, grades: [92, 95, 89], active: true },
+  { id: 3, name: "Pedro", age: 21, grades: [70, 65, 80], active: false },
+  { id: 4, name: "Ana", age: 18, grades: [60, 75, 68], active: true },
+  { id: 5, name: "Carlos", age: 22, grades: [90, 88, 92], active: false },
+];
+
+const studentsBelow70 = students.filter((student) => student.grades.some((grade) => grade < 70));
+
+console.log(studentsBelow70);
