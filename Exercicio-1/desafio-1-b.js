39cb1654b0023f32f2204fef3e55caadeae406b8
henrykhsa <henryk33658@gmail.com>
2025-08-12 21:49:15 -0300

    Refactor array combination function and add console output
    Updated the combinarArrays function to use .concat with spread for flexibility with any number of arrays. Added a console.log to display the combined result.

--- Exercicio-1/desafio-1-b.js
+++ Exercicio-1/desafio-1-b.js
@@ -1,13 +1,14 @@
 
 // b. Crie uma função que aceita múltiplos arrays como parâmetros e retorna um array combinado com todos os elementos (use rest e spread)
 
-
 const acais = ["Tradicional", "Trufado", "Guaraná"]
 const frutas = ["Morango", "Banana", "Manga"];
 const toppings = ["paçoca", "leite em pó", "Sucrilho"];
 
+//usando .concat
 function combinarArrays(...arrays) {
-  return [...arrays[0], ...arrays[1], ...arrays[2]];
+  return [].concat(...arrays);
 }
 
-const tigelaAcai = combinarArrays(acais, frutas, toppings);
+// exibindo os elementos combinados
+console.log(combinarArrays(acais, frutas, toppings));
